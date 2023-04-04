8bde758b08003244cc2f09f0beb80d290bfa8930
Kevin Castro <kevin.castro@familysearch.org>
2023-04-04 09:28:59 -0600

    testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState({
+    onHoverActivated: false,
+    onClickActivated: false,
+    onTypeActivated: false,
+    onFocusActivated: false,
+  });
+  const {
+    onHoverActivated,
+    onClickActivated,
+    onTypeActivated,
+    onFocusActivated,
+  } = state;
+
+  const handleOnHover = () => {
+    setState((current) => ({ ...current, onHoverActivated: true }));
+  };
+
+  const handleOnClick = () => {
+    setState((current) => ({ ...current, onClickActivated: true }));
+  };
+
+  const handleOnType = () => {
+    setState((current) => ({ ...current, onTypeActivated: true }));
+  };
+
+  const handleOnFocus = () => {
+    setState((current) => ({ ...current, onFocusActivated: true }));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <section>
+        <div>
+          <button
+            type='button'
+            data-testid='button-target'
+            onClick={handleOnClick}
+            onMouseOver={handleOnHover}
+          >
+            This will have hover and click events assigned!
+          </button>
+          <span data-testid='on-hover-span'>{onHoverActivated.toString()}</span>
+          <span data-testid='on-click-span'>{onClickActivated.toString()}</span>
+        </div>
+      </section>
+      <section>
+        <div>
+          <input
+            type='text'
+            onFocus={handleOnFocus}
+            onKeyDown={handleOnType}
+            data-testid='input-target'
+          />
+          <span data-testid='on-type-span'>{onTypeActivated.toString()}</span>
+          <span data-testid='on-focus-span'>{onFocusActivated.toString()}</span>
+        </div>
+      </section>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App container', () => {
+  const renderApp = () => render(<App />);
+
+  it('renders the app', () => {
+    renderApp();
+    expect(screen.getByTestId('on-hover-span')).toBeInTheDocument();
+  });
+});
+
+describe('button control', () => {
+  const renderApp = () => render(<App />);
+  const onClickValue = true;
+  const onHoverValue = true;
+
+  // it('clicks the button using fireEvent', () => {
+  //   renderApp();
+  //   fireEvent.click(screen.getByTestId('button-target'));
+
+  //   expect(screen.getByTestId('on-click-span')).toHaveTextContent(onClickValue);
+  //   expect(screen.getByTestId('on-hover-span')).toHaveTextContent(onHoverValue);
+  // });
+
+  it('clicks the button using userEvent', () => {
+    renderApp();
+    userEvent.click(screen.getByTestId('button-target'));
+
+    expect(screen.getByTestId('on-click-span')).toHaveTextContent(onClickValue);
+    expect(screen.getByTestId('on-hover-span')).toHaveTextContent(onHoverValue);
+  });
 });
